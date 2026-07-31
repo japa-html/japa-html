bd04a2326802db86467a3f5afbe96e7b91490320
japa-html <felipebaekr@gmail.com>
2026-07-31 16:36:01 -0300

    Add files via upload

---
+++ Gamernac/README.md
@@ -0,0 +1,4 @@
+# Gamernac - Loja de Games
+* Projeto Integrador da turma junho de 2026
+# Dados
+* cliente: João Button Silveira
---
+++ Gamernac/cabecalho.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Página Gamernac</title>
+	<meta name="author" content="Seu nome">
+	<meta name="keywords" content="php,jogos,hardware">
+	<meta name="description" content="Uma página sobre venda de jogos">
+	<link rel="icon" href="img/iconemus.png">
+	<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
